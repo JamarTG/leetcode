2b037494124850ce15dce8481aa12391caaf64cd
Jamari McFarlane <71823011+JamarTG@users.noreply.github.com>
2024-10-29 16:04:22 -0500

    Update 1299-Replace-Elements-with-Greatest-Element-on-Right-Side.py

--- 1299-Replace-Elements-with-Greatest-Element-on-Right-Side.py
+++ 1299-Replace-Elements-with-Greatest-Element-on-Right-Side.py
@@ -1,12 +1,16 @@
 class Solution:
     def replaceElements(self, arr: List[int]) -> List[int]:
+        # get running max
         running_max = -1
 
+        # traverse in reverse
         for i in range(len(arr)-1,-1,-1):
+            # overwrite value with largest so far
             tmp = arr[i]
             arr[i] = running_max
             if tmp > running_max:
                 running_max = tmp
+        # return new array
         return arr
         
 
