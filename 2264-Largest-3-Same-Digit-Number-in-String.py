e0db427327b8a43c08ea8cc9492f902abdbf43c7
Jamari McFarlane <71823011+JamarTG@users.noreply.github.com>
2024-10-29 16:09:09 -0500

    Update 2264-Largest-3-Same-Digit-Number-in-String.py

--- 2264-Largest-3-Same-Digit-Number-in-String.py
+++ 2264-Largest-3-Same-Digit-Number-in-String.py
@@ -2,10 +2,14 @@ class Solution:
     def largestGoodInteger(self, num: str) -> str:
         curr = ''
         
-        for i in range(2,len(num)):
-            if num[i] == num[i - 1] == num[i - 2] and num[i] >= curr:
-                curr = num[i]
 
+        # start at index where you have 2 values
+        for i in range(2,len(num)):
+            # ensure last 3 are equal and the num is greater than the prev number
+            if num[i] == num[i - 1] == num[i - 2] and num[i] >= curr:
+                # set current number as the max
+                curr = num[i]
+        # return the string  '777'
         return curr + curr + curr
         
         # print(curr)
