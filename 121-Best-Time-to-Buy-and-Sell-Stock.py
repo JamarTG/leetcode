715198c5cff56da65946fb6637cfe1ae6b7f3913
Jamari McFarlane <71823011+JamarTG@users.noreply.github.com>
2024-10-29 16:02:51 -0500

    Update 121-Best-Time-to-Buy-and-Sell-Stock.py

--- 121-Best-Time-to-Buy-and-Sell-Stock.py
+++ 121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -3,13 +3,16 @@ class Solution:
         max_profit = float('-inf')
         
         left = 0
+        # iterate through prices
         for right in range(len(prices)):
+            # calculate profit
             profit = prices[right]  - prices[left]
+            # update max profit
             max_profit = max(max_profit,profit)
 
+            # if unprofitable
             if profit < 0:
+                # set new lowest price index
                 left = right
-            
-        print(\reached here\,max_profit)       
-
+        # no negative profits
         return max(max_profit,0)
