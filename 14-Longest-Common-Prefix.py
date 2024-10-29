34f545d8c9008fa5d8af8ac5826c4861e4d3ef72
Jamari McFarlane <71823011+JamarTG@users.noreply.github.com>
2024-10-29 16:05:02 -0500

    Update 14-Longest-Common-Prefix.py

--- 14-Longest-Common-Prefix.py
+++ 14-Longest-Common-Prefix.py
@@ -1,12 +1,14 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        # start with the shortest string
         prefix = min(strs)
+        
         for st in strs:
             i = 0
             # while the characters are equal we increment i
             while i < len(prefix) and i < len(st) and prefix[i] == st[i]:
                 i+=1
-            # when that is not true, we take the characters upto that point
+            # when that is not true, we take the characters up to that point
             prefix = prefix[:i] 
         # return the prefix at the end
         return prefix
