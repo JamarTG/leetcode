0b1d2b59b18ecbe097df9e71afc49bf546bb1113
Jamari McFarlane <71823011+JamarTG@users.noreply.github.com>
2024-10-29 16:07:08 -0500

    Update 1512-Number-of-Good-Pairs.py

--- 1512-Number-of-Good-Pairs.py
+++ 1512-Number-of-Good-Pairs.py
@@ -3,10 +3,11 @@ from math import ceil
 
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
+        # get the character frequency
         counter = Counter(nums)
-
         res = 0
         for el in counter:
+            # (c * (c-1))// 2
             res += ceil(counter[el]  * (counter[el] - 1) / 2)
         return res
 
