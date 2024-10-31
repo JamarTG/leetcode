b7d97a6c2f88a4826e073481df6ce9ca2e4eb525
Jamari McFarlane <71823011+JamarTG@users.noreply.github.com>
2024-10-31 16:58:05 -0500

    [389] [Time Beats: 100.00%] [Memory Beats: 5.57%] - LeetPush

---
+++ 389-Find-the-Difference.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        x = 0
+
+        for i in s:
+            x ^= ord(i)
+        for i in t:
+            x ^= ord(i)
+            
+        return chr(x)
