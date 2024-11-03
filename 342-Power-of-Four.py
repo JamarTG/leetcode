77d05609de34e1970518e25a0b09eb6af5142b94
Jamari McFarlane <71823011+JamarTG@users.noreply.github.com>
2024-11-03 10:32:33 -0500

    [342] [Time Beats: 100.00%] [Memory Beats: 51.11%] - LeetPush

---
+++ 342-Power-of-Four.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        c = 0
+        even = True
+
+        if n < 4 and n != 1:
+            return False
+
+        while n:
+            a = (n & 1)
+            c += a
+
+            if (a == 1 and not even) or c > 1:
+                return False
+
+            even = not even
+            n >>= 1
+
+        return True
+
