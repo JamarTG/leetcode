548925c7217a44ea7f9937a0a3168bd22bb58cca
Jamari McFarlane <71823011+JamarTG@users.noreply.github.com>
2024-12-01 00:31:54 -0500

    [1337] [Time Beats: 100.00%] [Memory Beats: 5.44%] - LeetPush

--- 1337-The-K-Weakest-Rows-in-a-Matrix.py
+++ 1337-The-K-Weakest-Rows-in-a-Matrix.py
@@ -4,22 +4,16 @@ class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
         res = []
         for i in range(len(mat)):
-            strength = self.get_strength(mat[i])
-            heapq.heappush(res, (-strength, -i))  
+            heapq.heappush(res, (self.get_strength(mat[i]), i))
 
-            if len(res) > k:
-                heapq.heappop(res)
+        return [heapq.heappop(res)[-1] for i in range(k)]
 
-        return [abs(el[-1]) for el in sorted(res, reverse=True)]
-       
-    def get_strength(self,row):
-        left,right = 0,len(row)
-
+    def get_strength(self, row):
+        left, right = 0, len(row)
         while left < right:
             mid = (left + right) // 2
-        
             if row[mid] == 0:
-                right = mid 
+                right = mid
             else:
                 left = mid + 1
         return left
